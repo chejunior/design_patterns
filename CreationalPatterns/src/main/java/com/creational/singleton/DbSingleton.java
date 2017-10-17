e5f8bd3739a5f6f0788470a8473e6745a1988043
Maichel Castro <chejunior@MacBook-Air-de-Maichel.local>
2017-10-16 23:27:27 -0500

    Singleton pattern

--- CreationalPatterns/src/main/java/com/creational/singleton/DbSingleton.java
+++ CreationalPatterns/src/main/java/com/creational/singleton/DbSingleton.java
@@ -2,13 +2,21 @@ package com.creational.singleton;
 
 public class DbSingleton {
 	
-	private static DbSingleton instance = new DbSingleton();
+	private static DbSingleton instance = null;
 	
 	private DbSingleton() {
 		
 	}
 	
 	public static DbSingleton getInstance() {
+		
+		if (instance == null) {
+			synchronized (DbSingleton.class) {
+				if (instance == null) {
+					instance = new DbSingleton();
+				}
+			}
+		}
 		return instance;
 	}
 
