de50d8c16401e951b43e281990d451fa621e9d5f
Maichel Castro <chejunior@MacBook-Air-de-Maichel.local>
2017-10-16 23:00:21 -0500

    medium singleton

---
+++ CreationalPatterns/src/main/java/com/creational/singleton/DbSingletonDemo.java
@@ -0,0 +1,14 @@
+package com.creational.singleton;
+
+public class DbSingletonDemo {
+
+	public static void main(String[] args) {
+		DbSingleton instance = DbSingleton.getInstance();
+		DbSingleton anotherInstance = DbSingleton.getInstance();
+		
+		System.out.println(instance);
+		System.out.println(anotherInstance);
+
+	}
+
+}
